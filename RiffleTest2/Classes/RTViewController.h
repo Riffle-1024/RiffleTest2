738669aa0acca6bd2c0bdd8e0c84ad5ef318635f
Riffle-1024 <liuyalu1993@163.com>
2021-09-14 17:38:20 +0800

    提交代码

---
+++ RiffleTest2/Classes/RTViewController.h
@@ -0,0 +1,17 @@
+//
+//  RTViewController.h
+//  RiffleTest2_Example
+//
+//  Created by mxchip on 2021/9/14.
+//  Copyright © 2021 Riffle-1024. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RTViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RiffleTest2/Classes/RTViewController.m
@@ -0,0 +1,37 @@
+//
+//  RTViewController.m
+//  RiffleTest2_Example
+//
+//  Created by mxchip on 2021/9/14.
+//  Copyright © 2021 Riffle-1024. All rights reserved.
+//
+
+#import "RTViewController.h"
+
+@interface RTViewController ()
+
+@end
+
+@implementation RTViewController
+
++ (void)load
+{
+    NSLog(@"%s",__func__);
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
